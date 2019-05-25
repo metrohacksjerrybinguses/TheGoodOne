56d55a88767fa08f17c5e2640c2e7b22b1852d42
sarahlarusso <sjlarusso@wpi.edu>
2019-05-25 14:30:07 -0400

    Camera+Bitmap

--- app/src/main/java/com/example/thegoodone/MainActivity.java
+++ app/src/main/java/com/example/thegoodone/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.thegoodone;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+/*This page is the home page that opens when the user opens the app.
+From this page the user can take a picture of food
+ */
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private android.graphics.Bitmap imageBitmap;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCamera = findViewById(R.id.camera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCamera();
+            }
+        });
+    }
+
+    //Opens the camera
+    public void onCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    //Converts the image data to a String which can be stored in the app
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            //imageView.setImageBitmap(imageBitmap);
+        }
     }
 }

c53addf19160b82d6b1cf5cc0835073887030ba6
achintalapati <achintalapati@wpi.edu>
2019-05-25 21:49:06 -0400

    Scraper

---
+++ cookie/app/src/main/java/com/binguses/jerry/Scraper.java
@@ -0,0 +1,47 @@
+package com.binguses.jerry;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+public class Scraper {
+    String food;
+
+    public Scraper(String s){
+        food = s;
+    }
+
+    public double crawl(){
+        double calories = 0;
+
+        String url = "http://www.acaloriecounter.com/search/" + food.replace(' ','_');
+        Elements search = null;
+        try {
+            search = Jsoup.connect(url).get().select("a[href]");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String result = null;
+        for (Element link : search) {
+            if (link.attr("href").contains(food.replace(' ','-'))) {
+                result = link.attr("href");
+                break;
+            }
+        }
+        if (result != null) {
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(result).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            calories = Double.parseDouble(doc.select("li.calories").text().split(" ")[1]);
+            System.out.println(calories);
+        } else
+            System.out.println("No result found");
+
+        return calories;
+    }
+
+}

73e2d95e6dbb689cfacae4801319dc59879774db
XtremeUnction <kxdai@wpi.edu>
2019-05-25 22:19:09 -0400

    Connection to Checker

--- cookie/app/src/main/java/com/binguses/jerry/Calories.java
+++ cookie/app/src/main/java/com/binguses/jerry/Calories.java
@@ -19,10 +19,10 @@ public class Calories extends AppCompatActivity {
         TextView calories = (TextView) findViewById(R.id.calories);
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        int cal = (Integer)bundle.get("objCal");
+//        int cal = (Integer)bundle.get("objCal");
         String obj = bundle.getString("objName");
         textView.setText(obj);
-        calories.setText(cal);
+//        calories.setText(cal);
     }
 
     public void goToDay(View v){
--- cookie/app/src/main/java/com/binguses/jerry/Checker.java
+++ cookie/app/src/main/java/com/binguses/jerry/Checker.java
@@ -3,6 +3,8 @@ package com.binguses.jerry;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import org.tensorflow.lite.examples.classification.R;
@@ -25,13 +27,14 @@ public class Checker extends AppCompatActivity {
         textView.setText(list.get(0));
     }
 
-    public void goToCalories() {
+    public void goToCalories(View v) {
+        Log.d("ajajajj","jjjaaa");
         Intent intent = new Intent(this, Calories.class);
         intent.putExtra("objName", list.get(0));
         startActivity(intent);
     }
 
-    public void goToOptions() {
+    public void goToOptions(View v) {
         Intent intent = new Intent(this, Options.class);
         intent.putStringArrayListExtra("list", list);
         startActivity(intent);
